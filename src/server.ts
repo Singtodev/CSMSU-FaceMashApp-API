c0d40c6cfda2f1135202650bb1971b323c916420
Singtodev <138797225+Singtodev@users.noreply.github.com>
2024-02-23 16:28:41 +0700

    example express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,20 @@
-console.log("Hello world");
+import { Request , Response } from "express";
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get("/" , (req: Request , res: Response) => {
+    return res.send("facemash-app-api is worked!");
+})
+
+const PORT = process.env.PORT  || 8000;
+
+app.listen(PORT,() => {
+    console.log(`Server is running on port ${PORT}`)
+})
