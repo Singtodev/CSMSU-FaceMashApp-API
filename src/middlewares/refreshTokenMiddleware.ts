ea883790c7f5f0ee5152a2f2809c9bdeaab7013e
Singtodev <138797225+Singtodev@users.noreply.github.com>
2024-02-24 16:34:31 +0700

    set header

--- src/middlewares/refreshTokenMiddleware.ts
+++ src/middlewares/refreshTokenMiddleware.ts
@@ -1,23 +1,27 @@
 import { NextFunction, Request, Response } from "express";
 import { jwtService } from "../services";
 
-export const refreshTokenMiddleWare = async (req: Request, res: Response, next: NextFunction) => {
-    // Get the token from the request headers
-    const token =
-      req.headers.authorization && req.headers.authorization.split(" ")[1];
-  
-    if (!token) {
-      return next();
-    }
-  
-    try {
-      if (jwtService.isTokenExpired(token)) {
-        console.log("token หมดอายุแล้ว");
-        const newToken = jwtService.refreshToken(token);
-        res.setHeader("Authorization", `Bearer ${newToken}`);
-      }
-      next();
-    } catch (error: any) {
-      return res.status(401).json({ error: error.message });
-    }
+export const refreshTokenMiddleWare = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  // Get the token from the request headers
+  const token =
+    req.headers.authorization && req.headers.authorization.split(" ")[1];
+
+  if (!token) {
+    return next();
   }
+
+  try {
+    if (jwtService.isTokenExpired(token)) {
+      console.log("token หมดอายุแล้ว");
+      const newToken = jwtService.refreshToken(token);
+      return res.setHeader("Authorization", `Bearer ${newToken}`);
+    }
+    next();
+  } catch (error: any) {
+    return res.status(401).json({ error: error.message });
+  }
+};
