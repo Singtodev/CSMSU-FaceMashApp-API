86fcbb89b1702078f45971e214fe3fac65d1f8c5
Singtodev <138797225+Singtodev@users.noreply.github.com>
2024-03-02 16:23:35 +0700

    upload end point

--- src/controllers/firebase/index.ts
+++ src/controllers/firebase/index.ts
@@ -1,14 +1,80 @@
 import { Request, Response } from "express";
 import { jwtService } from "../../services";
+import path from "path";
 const express = require("express");
 
 const router = express.Router();
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  deleteObject,
+} from "firebase/storage";
+import multer from "multer";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCAGo0Z6pIJKDJkzuiAHjh6aB-3UZwsmBA",
+  authDomain: "facemash-5e1cf.firebaseapp.com",
+  projectId: "facemash-5e1cf",
+  storageBucket: "facemash-5e1cf.appspot.com",
+  messagingSenderId: "755415790965",
+  appId: "1:755415790965:web:c15dc45c65998e07624f3c",
+  measurementId: "G-CN11YLN6JJ",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+const storageFB = getStorage();
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
+  fileFilter: async (req, file, cb) => {
+    checkType(file, cb);
+  },
+}).single("image");
+
+const checkType = (file: any, cb: any) => {
+  const TypeAccepts = /jpeg|jpg|png|gif/;
+  const extName = TypeAccepts.test(
+    path.extname(file.originalname).toLowerCase()
+  );
+  const mimeType = TypeAccepts.test(file.mimetype);
+
+  if (mimeType && extName) {
+    return cb(null, true);
+  } else {
+    cb("Error: Image Type Accept JPG JPEG PNG GIF");
+  }
+};
+
+const uploadImage = async (file: any, quantity: any) => {
+  if (quantity === "single") {
+    const dateTime = Date.now();
+    const fileName = `images/${dateTime}_${Math.random() * 1000000}`;
+    const storageRef = ref(storageFB, fileName);
+    const metaData = {
+      contentType: file.type,
+    };
+
+    await uploadBytesResumable(storageRef, file.buffer, metaData);
+
+    return await getDownloadURL(storageRef);
+  }
+};
 
 router.get("/", (req: Request, res: Response) => {
   return res.send("Not found");
 });
 
-router.get("/upload", async (req: Request, res: Response) => {
+router.post("/upload", async (req: Request, res: Response) => {
   try {
     const { status, msg, data } = await jwtService.guardAuth(req, res);
     if (!status) {
@@ -17,13 +83,42 @@ router.get("/upload", async (req: Request, res: Response) => {
         msg,
       });
     }
-    return res.send("upload it work!");
+    
+    upload(req, res, async (err) => {
+      if (err instanceof multer.MulterError) {
+        // MulterError: File too large or other Multer errors
+        return res.status(400).json({ error: err.message });
+      } else if (err) {
+        // Other errors during file upload
+        return res
+          .status(500)
+          .json({ error: "Internal server error", msg: err });
+      }
+
+      if (!req.file) {
+        return res.status(400).json({ error: "No file uploaded" });
+      }
+
+      // Process the uploaded file
+      const file = {
+        type: req.file.mimetype,
+        buffer: req.file.buffer,
+      };
+
+      const urlImage = await uploadImage(file, "single");
+
+      // Send success response
+      return res.status(200).json({
+        url: urlImage,
+        msg: "File uploaded successfully!",
+      });
+    });
   } catch (err) {
     return res.status(500).send("Internal server error");
   }
 });
 
-router.get("/delete", async (req: Request, res: Response) => {
+router.post("/delete", async (req: Request, res: Response) => {
   try {
     const { status, msg, data } = await jwtService.guardAuth(req, res);
     if (!status) {
@@ -32,9 +127,24 @@ router.get("/delete", async (req: Request, res: Response) => {
         msg,
       });
     }
-    return res.send("delete it work!");
-  } catch (err) {
-    return res.status(500).send("Internal server error");
+
+    // Get the image URL to delete from the request body
+    const imageUrlToDelete = req.body.url;
+    if (!imageUrlToDelete) {
+      return res.status(400).json({ error: "Image URL is required" });
+    }
+
+    // Create a reference to the image in Firebase Storage
+    const imageRef = ref(storageFB, imageUrlToDelete);
+
+    // Delete the image from Firebase Storage
+    await deleteObject(imageRef);
+
+    // Send success response
+    return res.status(200).json({ msg: "Image deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting image:", error);
+    return res.status(500).json({ error: "Internal server error" });
   }
 });
 
